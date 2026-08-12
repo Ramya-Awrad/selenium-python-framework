5e1e269c5253bebdf2773c413ae784bd3677b6ae
Ramya Awrad <ramyaawrad9030@gmail.com>
2026-08-12 15:52:29 +0530

    Added logging utility and test logs

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,17 @@
 from pages.login_page import LoginPage
 from utils.config_reader import URL
 
+from utils.logger import logger
+
 def test_login(driver):
+    logger.info("Starting login test")
     driver.get(URL)
+    logger.info("Opened SauceDemo application")
+
 
     login_page = LoginPage(driver)
+    logger.info("LoginPage object created")
+    
     login_page.login("standard_user", "secret_sauce")
+    logger.info("Login completed successfully")
 
