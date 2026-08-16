c2286c1a339e88c6e35ad0c8b3c6dd1f04c00a2c
Ramya Awrad <ramyaawrad9030@gmail.com>
2026-08-16 11:10:20 +0530

    Add JSON test data support

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,16 +2,18 @@ from pages.login_page import LoginPage
 from utils.config_reader import URL
 
 from utils.logger import logger
+from utils.json_reader import read_json
 
 def test_login(driver):
     logger.info("Starting login test")
     driver.get(URL)
+    login_data = read_json("testdata/login_data.json")
     logger.info("Opened SauceDemo application")
 
 
     login_page = LoginPage(driver)
     logger.info("LoginPage object created")
     
-    login_page.login("standard_user", "secret_sauce")
+    login_page.login(login_data["username"], login_data["password"])
     logger.info("Login completed successfully")
 
